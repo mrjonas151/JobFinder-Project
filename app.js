62c0252f66ec0fa8d29c276ac44df0a56a8495e7
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-07-01 17:10:23 -0300

    feat: adding a new job route to jobs.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`Server running! PORT:${PORT}`)
 });
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 //db connection
 db.authenticate().then(()=>{
@@ -19,3 +23,6 @@ db.authenticate().then(()=>{
 app.get("/", (req, res) => {
     res.send("It's working!");
 })
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
---
+++ routes/jobs.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+// add job post
+router.post('/add', (req, res)=>{
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    //insert job BD
+    Job.create({
+        title,
+        salary,
+        company,
+        description,
+        email, 
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch( (err) => console.log("ERROR: " + err));
+});
+
+module.exports = router;
